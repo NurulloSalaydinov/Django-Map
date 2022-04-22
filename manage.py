6fde2aeff9204b22a0d148c2e5c01dcbd8ff412a
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-04-22 19:45:39 +0300

    manage.py updated

--- manage.py
+++ manage.py
@@ -7,14 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mapproject.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        )
+    from django.core.management import execute_from_command_line
 
 
 if __name__ == '__main__':
